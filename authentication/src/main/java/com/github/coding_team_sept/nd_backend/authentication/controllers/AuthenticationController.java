9d8360fedab6069950148894debe9ffc7f1035b9
nivratig <s3937114@student.rmit.edu.au>
2022-09-01 13:57:53 +1000

    Added postman

--- authentication/src/main/java/com/github/coding_team_sept/nd_backend/authentication/controllers/AuthenticationController.java
+++ authentication/src/main/java/com/github/coding_team_sept/nd_backend/authentication/controllers/AuthenticationController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpServerErrorException;
 
 @RestController
 @RequestMapping("api/v1")
