f1055fc265eb57915b4dbbe6e54b8b612142e446
nivratig <s3937114@student.rmit.edu.au>
2022-09-01 18:34:20 +1000

    Enabled pre and post and renamed security configuration class

--- authentication/src/main/java/com/github/coding_team_sept/nd_backend/authentication/configuration/SecurityConfig.java
+++ authentication/src/main/java/com/github/coding_team_sept/nd_backend/authentication/configuration/SecurityConfig.java
@@ -6,8 +6,10 @@ import com.github.coding_team_sept.nd_backend.authentication.services.AppUserDet
 import com.github.coding_team_sept.nd_backend.authentication.utils.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,9 +18,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
-
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AppUserDetailsService userDetailsService;
 
@@ -57,5 +60,4 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
     }
-
 }
