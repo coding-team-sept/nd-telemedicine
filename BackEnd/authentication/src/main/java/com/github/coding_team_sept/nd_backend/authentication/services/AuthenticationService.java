d5bcb28e8d3229a6227bda836d10902464be6a5b
xiaoming857 <gnimoaix857@gmail.com>
2022-09-19 20:22:28 +1000

    Removed unused imports

--- BackEnd/authentication/src/main/java/com/github/coding_team_sept/nd_backend/authentication/services/AuthenticationService.java
+++ BackEnd/authentication/src/main/java/com/github/coding_team_sept/nd_backend/authentication/services/AuthenticationService.java
@@ -3,9 +3,6 @@ package com.github.coding_team_sept.nd_backend.authentication.services;
 import com.github.coding_team_sept.nd_backend.authentication.enums.RoleType;
 import com.github.coding_team_sept.nd_backend.authentication.exceptions.AppException;
 import com.github.coding_team_sept.nd_backend.authentication.exceptions.EmailTakenException;
-import com.github.coding_team_sept.nd_backend.authentication.exceptions.format_exceptions.EmailFormatException;
-import com.github.coding_team_sept.nd_backend.authentication.exceptions.format_exceptions.PasswordFormatException;
-import com.github.coding_team_sept.nd_backend.authentication.exceptions.format_exceptions.UserNameFormatException;
 import com.github.coding_team_sept.nd_backend.authentication.models.AppUser;
 import com.github.coding_team_sept.nd_backend.authentication.models.AppUserDetails;
 import com.github.coding_team_sept.nd_backend.authentication.payloads.requests.LoginRequest;
@@ -15,14 +12,11 @@ import com.github.coding_team_sept.nd_backend.authentication.repositories.AppUse
 import com.github.coding_team_sept.nd_backend.authentication.repositories.RoleRepository;
 import com.github.coding_team_sept.nd_backend.authentication.utils.JwtUtils;
 import com.github.coding_team_sept.nd_backend.authentication.utils.ValidationUtils;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.regex.Pattern;
 
 @Service
 public record AuthenticationService(
