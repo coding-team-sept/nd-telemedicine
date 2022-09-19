d870ac9e225470f7280d1c2ee103dafcb580e9a5
xiaoming857 <gnimoaix857@gmail.com>
2022-09-19 18:44:09 +1000

    Improved app response

--- BackEnd/authentication/src/main/java/com/github/coding_team_sept/nd_backend/authentication/payloads/responses/AppResponse.java
+++ BackEnd/authentication/src/main/java/com/github/coding_team_sept/nd_backend/authentication/payloads/responses/AppResponse.java
@@ -5,37 +5,48 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.github.coding_team_sept.nd_backend.authentication.models.AppUserDetails;
 import lombok.Builder;
 
+import java.util.List;
 import java.util.Map;
 
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record AppResponse(Map<String, Object> data, Map<String, Object> error) {
+public record AppResponse(Object data, Object error) {
+    public static AppResponse error(Object error) {
+        return AppResponse.builder().error(error).build();
+    }
+
     public static AppResponse error(String message) {
-        return AppResponse.builder().error(Map.of(
-                "message", message
-        )).build();
+        return AppResponse.error(new ErrorResponse(message));
+    }
+
+    public static AppResponse data(Object data) {
+        return AppResponse.builder().data(data).build();
     }
 
     public static AppResponse register(String token) {
-        return AppResponse.builder().data(Map.of(
-                "token", Map.of(
-                        "token", token
-                )
-        )).build();
+        return AppResponse.data(Map.of(
+                "token", new TokenResponse(token)
+        ));
     }
 
     public static AppResponse login(String token, AppUserDetails userDetails) {
-        return AppResponse.builder().data(Map.of(
-                "token", Map.of(
-                        "token", token
-                ),
+        return AppResponse.data(Map.of(
+                "token", new TokenResponse(token),
                 "user", Map.of(
                         "id", userDetails.getId(),
                         "email", userDetails.getEmail(),
                         "name", userDetails.getName(),
                         "role", userDetails.getRole().getName().name()
                 )
-        )).build();
+        ));
+    }
+
+    public static AppResponse user(AppUserResponse appUserResponse) {
+        return AppResponse.data(appUserResponse);
+    }
+
+    public static AppResponse user(List<AppUserResponse> appUserResponses) {
+        return AppResponse.data(appUserResponses);
     }
 }
