64400739e0a0aec39c59eabf66c0a65db6ab8631
navneet555 <navneetsk@hotmail.com>
2022-09-20 16:10:32 +1000

    created Testing framework

--- BackEnd/authentication/src/test/java/SpringBootControllerTests.java
+++ BackEnd/authentication/src/test/java/SpringBootControllerTests.java
@@ -1,11 +1,14 @@
 import com.github.coding_team_sept.nd_backend.authentication.controllers.AuthenticationController;
 import com.github.coding_team_sept.nd_backend.authentication.enums.RoleType;
+import com.github.coding_team_sept.nd_backend.authentication.payloads.requests.LoginRequest;
 import com.github.coding_team_sept.nd_backend.authentication.payloads.requests.RegisterRequest;
 import com.github.coding_team_sept.nd_backend.authentication.payloads.responses.AppResponse;
 import com.github.coding_team_sept.nd_backend.authentication.services.AppUserService;
+import com.github.coding_team_sept.nd_backend.authentication.services.AuthenticationService;
 import org.apache.catalina.authenticator.SpnegoAuthenticator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +20,43 @@ import static org.mockito.Mockito.when;
 
 public class SpringBootControllerTests {
 
+    @Autowired
+    AuthenticationService authService;
+
     @Test
-    void addUserTest(){
-        AuthenticationController authController = new AuthenticationController();
-        RegisterRequest registerRequest = new RegisterRequest("admin@admin.com", "admin", "password");
-        ResponseEntity responseEntity = authController.addAdmin(registerRequest);
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        final var responseBody = responseEntity.getBody();
-        assertThat(responseEntity.name).isEqual(name);
-        assertThat(responseEntity.password).isEqual(password);
-        assertThat(responseEntity.email).isEqual(RoleType.ROLE_ADMIN);
+    void loginTest() {
+        authService.login(new LoginRequest("admin@admin.com", "password"));
+
+        //RegisterRequest registerRequest = new RegisterRequest("admin@admin.com", "admin", "password");
+        //ResponseEntity responseEntity = authController.addAdmin(registerRequest);
+        //assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+    @Test
+
+    void getUserByRoleTest(){
+        AppUserService appUserService = null;
+        appUserService.getUserByRole(RoleType.ROLE_ADMIN);
+    }
+
+    @Test
+    void getUserByIdTest(){
+
+    }
+    @Test
+    void getUserByIdsTest(){
+
+    }
+    @Test
+    void addAdminTest(){
+
+    }
+    @Test
+    void addDoctorTest(){
+
+    }
+    @Test
+    void registerPatientTest(){
+
     }
 
 }
