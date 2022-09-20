eaa73c4a73ebf7ed6fe09f1f9ca223eec755b264
Baiwei Liu <s3778909@student.rmit.edu.au>
2022-09-20 20:42:04 +1000

    Added login and add-doctor widget testing

--- FrontEnd/test/controller/add-doctor_test.dart
+++ FrontEnd/test/controller/add-doctor_test.dart
@@ -1,10 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:nd/app/modules/add-doctor/controllers/add_doctor_controller.dart';
+import 'package:nock/nock.dart';
 import 'package:test/test.dart';
 
 void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  group("Unit test for add doctor",
+          (){
+    late AddDoctorController controller;
+    setUp((){
+      Get.reset();
+      controller = AddDoctorController();
+    });
+
   test("Testing the doctor name is less than 4 characters and match the error hint",
           () {
-        final controller = AddDoctorController();
         controller.nameChanged("tes");
         try{
           controller.validate();
@@ -14,7 +25,6 @@ void main(){
 
   test("Testing the doctor name is entered correctly",
           () {
-        final controller = AddDoctorController();
         controller.nameChanged("test");
         try{
           controller.validate();
@@ -24,7 +34,6 @@ void main(){
 
   test("Testing the doctor email format on add doctor controller is valid",
           () {
-        final controller = AddDoctorController();
         controller.emailChanged("bai2");
         try{
           controller.validate();
@@ -33,7 +42,6 @@ void main(){
       });
   test("Testing if the doctor email format without @",
           () {
-        final controller = AddDoctorController();
         controller.emailChanged("bai2.com");
         try{
           controller.validate();
@@ -42,7 +50,6 @@ void main(){
       });
   test("Testing if the doctor email format enter correctly",
           () {
-        final controller = AddDoctorController();
         controller.emailChanged("bai2@bai.com");
         try{
           controller.validate();
@@ -61,7 +68,6 @@ void main(){
   });
   test("Testing if the doctor password on add doctor controller more than 24 characters",
           () {
-        final controller = AddDoctorController();
         controller.passwordChanged("1234564324325453253252352323523");
         try{
           controller.validate();
@@ -70,7 +76,6 @@ void main(){
       });
   test("Testing if the doctor password on add doctor controller is working",
           () {
-        final controller = AddDoctorController();
         controller.passwordChanged("12345678");
         try{
           controller.validate();
@@ -80,9 +85,24 @@ void main(){
 
   test("Testing if the loading status on add doctor controller is initially false",
           () {
-        final controller = AddDoctorController();
         expect(controller.isLoading.value, false);
       } );
 
+  test("Test add correct doctor", () async{
+    nock("http://10.0.2.2:9000/api/v1/admin/doctor")
+        .post("/")
+        .reply(201, null);
+    controller.token = "sdsadssa";
+    controller.nameChanged("testing");
+    controller.emailChanged("testing@testing.com");
+    controller.passwordChanged("12345678");
+    try {
+      controller.addDoctor();
+    } catch (_) {}
+    expect(controller.isLoading.value, true);
+    await Future.delayed(Duration(seconds: 2));
+    expect(controller.isLoading.value, false);
+  });
 
+  });
 }
