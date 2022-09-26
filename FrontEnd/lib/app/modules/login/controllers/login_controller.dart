b0304829d4dadede0651aa95d299ea058855edac
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2022-09-26 14:45:02 +1000

    Cleaned print statements and added email to storage

--- FrontEnd/lib/app/modules/login/controllers/login_controller.dart
+++ FrontEnd/lib/app/modules/login/controllers/login_controller.dart
@@ -6,7 +6,6 @@ import 'package:get/get.dart';
 import '../../../routes/app_pages.dart';
 
 class LoginController extends GetxController {
-
   final email = "".obs;
   final password = "".obs;
 
@@ -17,80 +16,72 @@ class LoginController extends GetxController {
 
   final isLoading = false.obs;
 
-  void toggleShowPassword(){
+  void toggleShowPassword() {
     showPassword.value = !showPassword.value;
   }
 
-  @override
-  void onInit() {
-    super.onInit();
-  }
+  void onEmailChange(String newEmail) => email.value = newEmail;
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  void onPasswordChange(String newPassword) => password.value = newPassword;
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void onEmailChange(String newEmail) =>email.value = newEmail;
-
-  void onPasswordChange(String newPassword) =>password.value = newPassword;
-
-  void doSignUp() async{
+  void doSignUp() async {
     isLoading.value = true;
-    await Future.delayed(Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 1));
 
     //send request to server
     var url = "http://10.0.2.2:9000/api/v1";
-    try{
-      var response = await Dio().post('$url/login', data:{
+    try {
+      var response = await Dio().post('$url/login', data: {
         'email': email.value,
         'password': password.value,
       });
       isLoading.value = false;
-      if(response.statusCode == 200){
-        print("success");
-        print(response.data["data"]["token"]["token"]);
-
+      if (response.statusCode == 200) {
         var storage = const FlutterSecureStorage();
-        await storage.write(key: 'token', value: response.data["data"]["token"]["token"]);
-        await storage.write(key: 'role', value: response.data["data"]["user"]["role"]);
+        await storage.write(
+            key: 'token', value: response.data["data"]["token"]["token"]);
+        await storage.write(
+            key: 'role', value: response.data["data"]["user"]["role"]);
+        await storage.write(key: 'email', value: email.value);
         Get.offNamedUntil(Routes.HOME, (route) => false);
-
-    }else{
-        Get.dialog(AlertDialog(title: Text("Error"), content: Text(response.data['error']['message']),));
-    }
-    }on DioError catch(e, _){
+      } else {
+        Get.dialog(AlertDialog(
+          title: const Text("Error"),
+          content: Text(response.data['error']['message']),
+        ));
+      }
+    } on DioError catch (e, _) {
       isLoading.value = false;
-      Get.dialog(AlertDialog(title: Text("Error"), content: Text(e.response?.data['error']['message']??"Unknown error!"),));
+      Get.dialog(AlertDialog(
+        title: const Text("Error"),
+        content: Text(e.response?.data['error']['message'] ?? "Unknown error!"),
+      ));
     }
   }
 
-  void login(){
+  void login() {
     bool success = true;
     emailError.value = "";
     passwordError.value = "";
 
-    bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email.value);
-    if (!emailValid){
+    bool emailValid = RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email.value);
+    if (!emailValid) {
       success = false;
       emailError.value = "Please enter a valid email";
     }
 
-    if (password.value.isEmpty){
+    if (password.value.isEmpty) {
       success = false;
       passwordError.value = "Password cannot be empty";
     }
-    if (password.value.length < 8 || password.value.length > 24){
+    if (password.value.length < 8 || password.value.length > 24) {
       success = false;
       passwordError.value = "Password should be 8 to 24 characters";
     }
 
-    if (!success){
+    if (!success) {
       Get.dialog(const AlertDialog(
         title: Text("Sign In Failed"),
         content: Text("Please check your email and password"),
@@ -98,11 +89,9 @@ class LoginController extends GetxController {
       return;
     }
     doSignUp();
-
   }
 
-  void signUp(){
+  void signUp() {
     Get.offNamedUntil(Routes.REGISTER, (r) => false);
   }
-
 }
