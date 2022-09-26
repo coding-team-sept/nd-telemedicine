0ba9d481657b89765ea6ec2315eb0cf039e7e292
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2022-09-26 14:30:15 +1000

    Added email to storage & fixed error message

--- FrontEnd/lib/app/modules/register/controllers/register_controller.dart
+++ FrontEnd/lib/app/modules/register/controllers/register_controller.dart
@@ -5,14 +5,9 @@ import 'package:get/get.dart';
 import 'package:nd/app/routes/app_pages.dart';
 
 class RegisterController extends GetxController {
-
-
-  void login() async{
-   Get.offNamedUntil(Routes.LOGIN, (route) => false);
-
+  void login() async {
+    Get.offNamedUntil(Routes.LOGIN, (route) => false);
   }
-
-
 
   final fullName = "".obs;
   final email = "".obs;
@@ -37,21 +32,6 @@ class RegisterController extends GetxController {
     showConfirmPassword.value = !showConfirmPassword.value;
   }
 
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
   void finishSignUp() async {
     isLoading.value = true;
     await Future.delayed(const Duration(seconds: 1));
@@ -66,18 +46,15 @@ class RegisterController extends GetxController {
       });
       isLoading.value = false;
       if (response.statusCode == 201) {
-        print('success');
-        print(response.data['data']['token']['token']);
         const storage = FlutterSecureStorage();
-        await storage.write(key: 'token',value: response.data['data']['token']['token']);
-        await storage.write(key: 'role',value: 'patient');
+        await storage.write(
+            key: 'token', value: response.data['data']['token']['token']);
+        await storage.write(key: 'role', value: 'patient');
+        await storage.write(key: 'email', value: email.value);
         Get.offNamedUntil(Routes.HOME, (route) => false);
-
-
-
       } else {
         Get.dialog(AlertDialog(
-          title: Text('Error'),
+          title: const Text('Error'),
           content: Text(response.data['error']['message']),
         ));
       }
@@ -85,8 +62,8 @@ class RegisterController extends GetxController {
     } on DioError catch (e, _) {
       isLoading.value = false;
       Get.dialog(AlertDialog(
-        title: Text('Error'),
-        content: Text(e.response?.data['error']['message'] ?? 'Unknown data'),
+        title: const Text('Error'),
+        content: Text(e.response?.data['error']['message'] ?? 'Unknown error'),
       ));
     }
   }
@@ -126,8 +103,6 @@ class RegisterController extends GetxController {
       emailError.value = "Please input a valid email";
       success = false;
     }
-
-
 
     if (password.value.length < 8 || password.value.length > 24) {
       passwordError.value = "Password length must 8-24 characters";
