be100bbdb28198110140a0a7a03648804d13e4db
Baiwei Liu <s3778909@student.rmit.edu.au>
2022-09-26 18:11:22 +1000

    create doctor manager page to display all the doctors

--- FrontEnd/lib/app/modules/home/bindings/home_binding.dart
+++ FrontEnd/lib/app/modules/home/bindings/home_binding.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 
 import 'package:nd/app/modules/home/controllers/appointment_controller.dart';
+import 'package:nd/app/modules/home/controllers/doctor_manager_controller.dart';
 import 'package:nd/app/modules/home/controllers/profile_controller.dart';
 
 import '../controllers/home_controller.dart';
@@ -8,6 +9,9 @@ import '../controllers/home_controller.dart';
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<DoctorManagerController>(
+      () => DoctorManagerController(),
+    );
     Get.lazyPut<ProfileController>(() => ProfileController());
     Get.put<HomeController>(HomeController());
     Get.lazyPut<AppointmentController>(() => AppointmentController());
