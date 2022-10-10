c210680223fec51390245e02b6a9ef84ddc0b51c
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2022-10-10 15:21:23 +1100

    Added newest endpoint

--- FrontEnd/lib/app/data/const.dart
+++ FrontEnd/lib/app/data/const.dart
@@ -6,7 +6,7 @@ class C {
   // static const url = "http://95.111.217.168:9000/api/v1";
   // static const urlA = "http://95.111.217.168:9001/api/v1";
   // static const urlC = "http://95.111.217.168:9002/api/v1";
-  static const url = "http://10.0.2.2:9000/api/v1";
-  static const urlA = "http://10.0.2.2:9001/api/v1";
-  static const urlC = "http://10.0.2.2:9002/api/v1";
+  static const url = "http://95.111.217.168/auth/api/v1";
+  static const urlA = "http://95.111.217.168/appointment/api/v1";
+  static const urlC = "http://95.111.217.168/chat/api/v1";
 }
