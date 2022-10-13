0fc7b3be9e426428c8a51e257b19ffda3e642c93
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2022-10-13 12:40:05 +1100

    Added additional tests for login using dio mock

--- FrontEnd/lib/app/modules/home/views/home_view.dart
+++ FrontEnd/lib/app/modules/home/views/home_view.dart
@@ -76,31 +76,28 @@ class HomeView extends GetView<HomeController> {
           appBar: AppBar(
             leading: Image.asset("assets/icon.jpg"),
             title: Text(controller.email.value ?? ""),
-            bottom: controller.role.value == null
-                ? PreferredSize(
-                    preferredSize: const Size.fromHeight(0), child: Container())
-                : TabBar(tabs: [
-                    if (controller.role.value == "ROLE_ADMIN") ...adminTabBar,
-                    if (controller.role.value == "ROLE_PATIENT" ||
-                        controller.role.value == "patient")
-                      ...patientTabBar,
-                    if (controller.role.value == "ROLE_DOCTOR") ...doctorTabBar,
-                    const Tab(
-                      icon: Icon(Icons.account_circle),
-                      text: "Profile",
-                    ),
-                  ]),
+            bottom: TabBar(tabs: [
+              if (controller.role.value == "ROLE_ADMIN") ...adminTabBar,
+              if (controller.role.value == "ROLE_PATIENT" ||
+                  controller.role.value == "patient")
+                ...patientTabBar,
+              if (controller.role.value == "ROLE_DOCTOR") ...doctorTabBar,
+              if(controller.role.value == null) ...doctorTabBar,
+              const Tab(
+                icon: Icon(Icons.account_circle),
+                text: "Profile",
+              ),
+            ]),
           ),
-          body: controller.role.value == null
-              ? const Center(child: CircularProgressIndicator())
-              : TabBarView(children: [
-                  if (controller.role.value == "ROLE_PATIENT" ||
-                      controller.role.value == "patient")
-                    ...patientContent,
-                  if (controller.role.value == "ROLE_ADMIN") ...adminContent,
-                  if (controller.role.value == "ROLE_DOCTOR") ...doctorContent,
-                  const ProfileView(),
-                ]),
+          body: TabBarView(children: [
+            if (controller.role.value == "ROLE_PATIENT" ||
+                controller.role.value == "patient")
+              ...patientContent,
+            if (controller.role.value == "ROLE_ADMIN") ...adminContent,
+            if (controller.role.value == "ROLE_DOCTOR") ...doctorContent,
+            if(controller.role.value == null) ...emptyContent,
+            const ProfileView(),
+          ]),
         ),
       ),
     );
