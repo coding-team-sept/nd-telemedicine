e7e2a3cc2a455ee90808df90ca9ed9465d46cd2f
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2022-10-18 15:28:28 +1100

    updated ssh commands

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,8 @@
 chmod 0600 ./deploy/sept
 
 # Reset the server
-ssh -i ./deploy/sept root@95.111.217.168 './reset.sh'
+ssh -i ./deploy/sept -o StrictHostKeyChecking=no root@95.111.217.168 './reset.sh'
 # Copy file
-scp -i ./deploy/sept -rp ../BackEnd root@95.111.217.168:/root
+scp -i ./deploy/sept -rp ../BackEnd -o StrictHostKeyChecking=no root@95.111.217.168:/root
 # start docker compose
-ssh -i ./deploy/sept root@95.111.217.168 './run.sh'
+ssh -i ./deploy/sept -o StrictHostKeyChecking=no root@95.111.217.168 './run.sh'
