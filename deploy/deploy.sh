a64f5ec259f61346936d24184c190553fb60f584
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2022-10-18 15:09:18 +1100

    Updated CD"

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,8 @@
-chmod 0600 ./sept
+chmod 0600 ./deploy/sept
 
 # Reset the server
-ssh -i ./sept root@95.111.217.168 './reset.sh'
+ssh -i ./deploy/sept root@95.111.217.168 './reset.sh'
 # Copy file
-scp -i ./sept -rp ../BackEnd root@95.111.217.168:/root
+scp -i ./deploy/sept -rp ../BackEnd root@95.111.217.168:/root
 # start docker compose
-ssh -i ./sept root@95.111.217.168 './run.sh'
+ssh -i ./deploy/sept root@95.111.217.168 './run.sh'
